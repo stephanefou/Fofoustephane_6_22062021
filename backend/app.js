6d701df8703180e877c379238792f3cfe3912117
Stephane Fou <t-fane95@hotmail.fr>
2021-07-30 17:35:49 +0200

    config of data and user schema and with mongoose

---
+++ backend/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb+srv://stephane:Fm2o5A3g7uCaKhTX@cluster0.ciij3.mongodb.net/store?retryWrites=true&w=majority',
+        { useNewUrlParser: true,
+        useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+    
+const app = express();
+
+app.use((req, res, next) => {
+  console.log('Requête reçue !');
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json({ message: 'Votre requête a bien été reçue !' });
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('Réponse envoyée avec succès !');
+});
+
+module.exports = app;
---
+++ backend/models/Sauce.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+
+const sauceSchema = mongoose.Schema({
+    /* id: ObjectID — identifiant unique créé par MongoDB */
+    userId: { type: String, required: true }, /* identifiant unique MongoDB pour l'utilisateur qui a créé la
+    sauce */
+    name: { type: String, required: true }, /* nom de la sauce */
+    manufacturer: { type: String, required: true }, /* fabricant de la sauce */
+    description: { type: String, required: true }, /* description de la sauce */
+    mainPepper: { type: String, required: true }, /* principal ingrédient dans la sauce */
+    imageUrl: { type: String, required: true }, /* string de l'image de la sauce téléchargée par l'utilisateur */
+    heat: { type: Number, required: true }, /* nombre entre 1 et 10 décrivant la sauce */
+    likes: { type: Number, required: true }, /* nombre d'utilisateurs qui aiment la sauce */
+    dislikes: { type: Number, required: true }, /* nombre d'utilisateurs qui n'aiment pas la sauce */
+    usersLiked: { type: [String], required: true }, /* tableau d'identifiants d'utilisateurs ayant aimé la sauce */
+    usersDisliked: { type: [String], required: true }, /* tableau d'identifiants d'utilisateurs n'ayant pas aimé
+    la sauce */
+});
+
+
+module.exports = mongoose.model('Sauce', sauceSchema);
